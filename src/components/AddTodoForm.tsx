4883c382e9f186b2091040fd4a7e397b49f7488c
haidar180202 <gshaidar6@gmail.com>
2024-04-09 01:07:13 +0700

    push for update the application project

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -8,7 +8,7 @@ import {
   Button
 } from '@nextui-org/react';
 import { Input } from '@nextui-org/react';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '@/redux/store';
 import { Checkbox } from '@nextui-org/react';
 import { addTodo, setMoveLocationTodo, } from '@/redux/features/todo-slice';
@@ -33,66 +33,66 @@ const AddTodoForm: React.FC = () => {
   };
 
   const { list } = useSelector((state: RootState) => state.todoReducer);
-  
-  const handleMoveAndRemove = (id:number,name:string) => {
+
+  const handleMoveAndRemove = (id: number, name: string) => {
     // Kirim nilai todo.name ke Redux (Anda dapat menggantinya sesuai kebutuhan)
-    console.log(id,name);
-    dispatch(setMoveLocationTodo({ id: id, name: name ,jumlah:0 }));
+    console.log(id, name);
+    dispatch(setMoveLocationTodo({ id: id, name: name, jumlah: 0 }));
     // Di sini Anda bisa melakukan apa pun yang ingin Anda lakukan ketika tombol diklik
   };
-  
+
   return (
     <>
-    <Card className="p-4">
-      {/* <CardHeader>
+      <Card className="p-4">
+        {/* <CardHeader>
         <div className="flex flex-col">
           <p className="text-lg font-bold">Todo List</p>
           <p className="text-small text-default-500">Manage your tasks</p>
         </div>
       </CardHeader> */}
-      <CardBody>
-  {list.map((todo) => (
-    <div className="flex mb-2 justify-center" key={todo.id}>
-      <div>
-        <button className="font-bold pb-1 px-4 rounded" onClick={() => handleMoveAndRemove(todo.id, todo.name)}>
-          {todo.name}
-        </button>
-      </div>
-    </div>
-  ))}
-  <div className="flex mt-8 justify-center font-bold">
-    (List bahan-bahan)
-  </div>
-</CardBody>
+        <CardBody>
+          {list.map((todo) => (
+            <div className="flex mb-2 justify-center" key={todo.id}>
+              <div>
+                <button className="font-bold pb-1 px-4 rounded" onClick={() => handleMoveAndRemove(todo.id, todo.name)}>
+                  {todo.name}
+                </button>
+              </div>
+            </div>
+          ))}
+          <div className="flex mt-8 justify-center font-bold">
+            (List bahan-bahan)
+          </div>
+        </CardBody>
 
 
-      <CardHeader>
-        <div className="flex flex-col">
-          <p className="text-lg font-bold">Add Todo</p>
-          <p className="text-small text-default-500">Type and save your task</p>
-        </div>
-      </CardHeader>
-      <CardBody>
-        <Input
-          isRequired
-          label="Task Name"
-          value={todo}
-          onValueChange={setTodo}
-          errorMessage={errorMessage}
-          variant="bordered"
-        />
-      </CardBody>
+        <CardHeader>
+          <div className="flex flex-col">
+            <p className="text-lg font-bold">Add Todo</p>
+            <p className="text-small text-default-500">Type and save your task</p>
+          </div>
+        </CardHeader>
+        <CardBody>
+          <Input
+            isRequired
+            label="Task Name"
+            value={todo}
+            onValueChange={setTodo}
+            errorMessage={errorMessage}
+            variant="bordered"
+          />
+        </CardBody>
 
-      <CardFooter>
-        <Button color="primary" variant="solid" onClick={() => handleSubmit()}>
-          Add
-        </Button>
-      </CardFooter>
-    </Card>
+        <CardFooter>
+          <Button color="primary" variant="solid" onClick={() => handleSubmit()}>
+            Add
+          </Button>
+        </CardFooter>
+      </Card>
 
     </>
-   
-    
+
+
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,30 +1,29 @@
 'use client';
 import React, { useRef, useEffect, useReducer } from 'react';
-import { Card, CardHeader, CardBody } from '@nextui-org/react';
+import { Card, CardHeader, CardBody,Input } from '@nextui-org/react';
 import { SetMoveremoveTodo, setManipulateStatusCard, setManipulateStatusJumlahBahan, setManipulateTatacara, setMoveDataAddForTatacara, setMoveUpdateData, setMoveUpdateKeteranganTatacaraData, } from '@/redux/features/todo-slice';
 import { AppDispatch, RootState } from '@/redux/store';
 import { useDispatch, useSelector } from 'react-redux';
-import { Checkbox } from '@nextui-org/react';
-import { BsFillTrashFill,BsPencil } from 'react-icons/bs';
-import { current } from '@reduxjs/toolkit';
+import { BsFillTrashFill, BsPencil } from 'react-icons/bs';
+
 
 const TodoList: React.FC = () => {
   const clickedIndexRef = useRef(null); // for setting konfigurasi saat card klik bagian bahan maka akan difungsikan untuk hide dan show button
   const clickedIndexRefForEditTatacara = useRef(null); // for setting edit status konfigurasi saat data keterngan tatacara di klik maka bagian keterangan tatacara dapat di ubah selalu 
   const clickedIndexRefForEditJumlah = useRef(null); // for setting edit status konfigurasi saat icon edit di klik bagian jumlah bahan dapat diubah
 
-  const { moveLocationTodo, tatacara, StatusKlikCard,StatusKlikManipulasiTatacara,StatusKlikManipulasiJumlahBahanResep } = useSelector((state: RootState) => state.todoReducer);
+  const { moveLocationTodo, tatacara, StatusKlikCard, StatusKlikManipulasiTatacara, StatusKlikManipulasiJumlahBahanResep } = useSelector((state: RootState) => state.todoReducer);
   const dispatch = useDispatch<AppDispatch>();
 
   const handleMoveRemoveDataMove = (id: number, name: string) => {
     // Kirim nilai todo.name ke Redux (Anda dapat menggantinya sesuai kebutuhan)
     console.log(id, name);
-    dispatch(SetMoveremoveTodo({ id: id, name: name}));
+    dispatch(SetMoveremoveTodo({ id: id, name: name }));
     // Di sini Anda bisa melakukan apa pun yang ingin Anda lakukan ketika tombol diklik
   };
 
-  const handleKeyDown = (event: any, id: number) => {
-    if (event.key === 'Enter' && event.target.value >= 1 || (event.type === 'blur' &&  event.target.value >= 1)) {
+  const handleKeyUpdateJumlahForFirst = (event: any, id: number) => {
+    if (event.key === 'Enter' && event.target.value >= 1 || (event.type === 'blur' && event.target.value >= 1)) {
       let weightInput = event.target.value;
       dispatch(setMoveUpdateData({ id: id, jumlah: weightInput }));
 
@@ -33,8 +32,8 @@ const TodoList: React.FC = () => {
     }
   };
 
-  const handleKeyUpdate = (event: any, id: number) => {
-    if (event.key === 'Enter' && event.target.value >= 1 || (event.type === 'blur' &&  event.target.value >= 1)) {
+  const handleKeyUpdateJumlah = (event: any, id: number) => {
+    if (event.key === 'Enter' && event.target.value >= 1 || (event.type === 'blur' && event.target.value >= 1)) {
       let weightInput = event.target.value;
       dispatch(setMoveUpdateData({ id: id, jumlah: weightInput }));
       dispatch(setManipulateStatusJumlahBahan(false));
@@ -45,7 +44,7 @@ const TodoList: React.FC = () => {
   };
 
   const handleAddKeterangan = (event: any) => {
-    if (event.key === 'Enter' && event.target.value !== "" || (event.type === 'blur' &&  event.target.value !== "")) {
+    if (event.key === 'Enter' && event.target.value !== "" || (event.type === 'blur' && event.target.value !== "")) {
       const input = event.target;
       const keterangan = input.value.trim();
 
@@ -56,10 +55,10 @@ const TodoList: React.FC = () => {
     }
   };
 
-  const handleButtonClick = (index:any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
+  const handleValidasiStatusButtonClick = (index: any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
     clickedIndexRef.current = index;
     dispatch(setManipulateStatusCard(true));
-    
+
     // Set timeout untuk menyembunyikan kembali data setelah 3 menit
     setTimeout(() => {
       dispatch(setManipulateStatusCard(false));
@@ -68,78 +67,67 @@ const TodoList: React.FC = () => {
   }
 
 
-const handleForSettingWaktuJumlah  = (index:any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
+  const handleForSettingValidasiStatusJumlah = (index: any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
     clickedIndexRefForEditJumlah.current = index;
     dispatch(setManipulateStatusJumlahBahan(true));
     // Set timeout untuk menyembunyikan kembali data setelah 3 menit
     // 180000 milidetik = 3 menit
   }
 
-  const handleForSettingWaktuTatacara  = (index:any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
+  const handleForSettingValidasiStatusTatacara = (index: any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
     clickedIndexRefForEditTatacara.current = index;
     dispatch(setManipulateTatacara(true));
     // Set timeout untuk menyembunyikan kembali data setelah 3 menit
-     // 180000 milidetik = 3 menit
+    // 180000 milidetik = 3 menit
   }
 
-  const handleUpdateTatacara  = (event:any,id:any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
-   if(event.key === 'Enter' && event.target.value !== "" || event.type === 'blur' &&  event.target.value !== ""){
-    let weightInput = event.target.value;
+  const handleUpdateTatacara = (event: any, id: any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
+    if (event.key === 'Enter' && event.target.value !== "" || event.type === 'blur' && event.target.value !== "") {
+      let weightInput = event.target.value;
       dispatch(setMoveUpdateKeteranganTatacaraData({ id: id, keterangan: weightInput }));
       dispatch(setManipulateTatacara(false));
-   }
-    
-  }
+    }
 
-  const handleUpdateJumlah  = (event:any,id:any) => { // fungsi untuk setting perubahan status saat card bahan diklik maka akan menampilkan buttonnya lalu menghide berdasarkan waktu tertentu secara otomatis saat tidak di klik
-    // if(event.key === 'Enter' && event.target.value !== "" || event.type === 'blur' &&  event.target.value >= 1){
-    //  let weightInput = event.target.value;
-    //  console.log(weightInput);
-    //   //  dispatch(setMoveUpdateData({ id: id, keterangan: weightInput }));
-    //   //  dispatch(setManipulateJumlah(false));
-    // }
-    console.log(event,id,"teken trus");
-     
-   }
-  
+  }
 
 
   return (
     <Card className="p-4">
-      <CardHeader>
-        <div className="flex flex-col">
+      <CardHeader className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
           <p className="text-lg font-bold text-center">Nama Resep (Input)</p>
         </div>
       </CardHeader>
+
       <CardBody>
         <p className="text-md font-bold">Bahan :</p>
         {moveLocationTodo.map((todo, index) => (
-          <button key={index} onClick={() => handleButtonClick(index)}>
+          <button key={index} onClick={() => handleValidasiStatusButtonClick(index)}>
             <div className="flex mb-2 justify-between" key={todo.id}>
               <div>
 
                 <p>
-                  {todo.jumlah === 0  ? (<input type="number" min={1} className="mx-2 w-10 py-2 mt-1" style={{ textAlign: 'center' }} onKeyDown={(event) => handleKeyDown(event, todo.id)} onBlur={(event) => handleKeyDown(event, todo.id)} defaultValue={todo.jumlah >= 1 ? todo.jumlah : ''}/>) :  StatusKlikManipulasiJumlahBahanResep.status === true && clickedIndexRefForEditJumlah.current === index ? (<input type="number" min={1} className="mx-2 w-10 py-2 mt-1" style={{ textAlign: 'center' }} onKeyDown={(event) => handleKeyUpdate(event, todo.id)} onBlur={(event) => handleKeyUpdate(event, todo.id)} defaultValue={todo.jumlah >= 1 ? todo.jumlah : ''}/>) :(<>{todo.jumlah}</>) }{' '} kg <span className='ml-3'>{todo.name}</span>
+                  {todo.jumlah === 0 ? (<input type="number" min={1} className="mx-2 w-10 py-2 mt-1 rounded bg-slate-300 " style={{ textAlign: 'center' }} onKeyDown={(event) => handleKeyUpdateJumlahForFirst(event, todo.id)} onBlur={(event) => handleKeyUpdateJumlahForFirst(event, todo.id)} defaultValue={todo.jumlah >= 1 ? todo.jumlah : ''} />) : StatusKlikManipulasiJumlahBahanResep.status === true && clickedIndexRefForEditJumlah.current === index ? (<input type="number" min={1} className="mx-2 w-10 py-2 mt-1 rounded bg-slate-300 " style={{ textAlign: 'center' }} onKeyDown={(event) => handleKeyUpdateJumlah(event, todo.id)} onBlur={(event) => handleKeyUpdateJumlah(event, todo.id)} defaultValue={todo.jumlah >= 1 ? todo.jumlah : ''} />) : (<>{todo.jumlah}</>)}{' '} kg <span className='ml-3'>{todo.name}</span>
                 </p>
               </div>
 
               {StatusKlikCard.status === true && clickedIndexRef.current === index && (
-              <div className="flex space-x-1">
-                <div className="my-auto">
-                  <BsPencil
-                    className="cursor-pointer text-[18px] mr-3"
-                    onClick={() => handleForSettingWaktuJumlah(index)}
+                <div className="flex space-x-1">
+                  <div className="my-auto">
+                    <BsPencil
+                      className="cursor-pointer text-[18px] mr-3"
+                      onClick={() => handleForSettingValidasiStatusJumlah(index)}
                     // onChange={() => dispatch(toggleTodo(todo.id))}
-                  />
+                    />
+                  </div>
+                  <div className="my-auto">
+                    <BsFillTrashFill
+                      className="cursor-pointer text-[18px]"
+                      onClick={() => handleMoveRemoveDataMove(todo.id, todo.name)}
+                    />
+                  </div>
                 </div>
-                <div className="my-auto">
-                  <BsFillTrashFill
-                    className="cursor-pointer text-[18px]"
-                    onClick={() => handleMoveRemoveDataMove(todo.id, todo.name)}
-                  />
-                </div>
-              </div>
-            )}
+              )}
             </div>
           </button>
         ))}
@@ -150,23 +138,25 @@ const handleForSettingWaktuJumlah  = (index:any) => { // fungsi untuk setting pe
 
             {moveLocationTodo.length > 0 && tatacara.map((dd, index) => (
               <div key={dd.id}>
-                {StatusKlikManipulasiTatacara.status === true && clickedIndexRefForEditTatacara.current === index ? (  
-                   <input
-                   type="text"
-                   defaultValue={dd.keterangan} // Mengisi nilai default input dengan keterangan sebelumnya
-                   onBlur={(event) => handleUpdateTatacara(event, dd.id)} // Menangani update saat input blur
-                   onKeyDown={(event) => {handleUpdateTatacara(event, dd.id)}
-                  }
-                 />
+                {StatusKlikManipulasiTatacara.status === true && clickedIndexRefForEditTatacara.current === index ? (
+                  <input
+                    className='pl-2 py-1 mt-1 rounded bg-slate-300'
+                    type="text"
+                    defaultValue={dd.keterangan} // Mengisi nilai default input dengan keterangan sebelumnya
+                    onBlur={(event) => handleUpdateTatacara(event, dd.id)} // Menangani update saat input blur
+                    onKeyDown={(event) => { handleUpdateTatacara(event, dd.id) }
+
+                    }
+                  />
                 ) : (
-                  <button onClick={()=>handleForSettingWaktuTatacara(index)}>
-                  {index + 1}.{dd.keterangan}
+                  <button onClick={() => handleForSettingValidasiStatusTatacara(index)}>
+                    {index + 1}. {dd.keterangan}
                   </button>
                 )}
-                
+
               </div>
             ))}
-            <input type={moveLocationTodo.length === 0 ? 'disabled' : 'text'} className="mx-2 py-1 mt-1" disabled={moveLocationTodo.length === 0} onKeyDown={(event) => handleAddKeterangan(event)}  onBlur={(event) => handleAddKeterangan(event)}/>
+            <input type={moveLocationTodo.length === 0 ? 'disabled' : 'text'} className="pl-2 py-1 mt-2 rounded bg-slate-300" disabled={moveLocationTodo.length === 0} onKeyDown={(event) => handleAddKeterangan(event)} onBlur={(event) => handleAddKeterangan(event)} />
           </div>
         </div>
 
